84b6f58b66a4c52f602bb9a63ac3a6c2bb48ef48
cute <cenegd@live.com>
2019-01-20 14:53:10 +0800

    Beta Release

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,8 @@
 </head>
 <body>
     <div id="app"></div>
+    <script type="text/javascript" src="{{ elixir('js/manifest.js') }}"></script>
+    <script type="text/javascript" src="{{ elixir('js/vendor.js') }}"></script>
     <script type="text/javascript" src="{{ elixir('js/app.js') }}"></script>
 </body>
 </html>
