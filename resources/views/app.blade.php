bb25b0568dc1c056dbdef75ebbbfa9f5f34b88f8
cute <cenegd@live.com>
2019-04-09 18:52:56 +0800

    Optimize template resource reference methods

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>{{ env('APP_NAME') }}</title>
+    <title>{{ config('app.name') }}</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" type="text/css" href="{{ elixir('css/app.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
 </head>
 <body>
     <div id="app"></div>
-    <script type="text/javascript" src="{{ elixir('js/app.js') }}"></script>
+    <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
