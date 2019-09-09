0a7fc86a50a726a75ffb05d6d7d80442c2eb4725
cute <cenegd@live.com>
2019-09-09 13:07:19 +0800

    Remove server image optimize

--- app/Attachment.php
+++ app/Attachment.php
@@ -14,18 +14,14 @@ class Attachment extends Model
     // 存储文件到磁盘
     public function store($attachment)
     {
-        $filename = strtolower(str_random(32) . '.jpg');
-
-        $manager = new ImageManager();
-        $image = $manager->make($attachment['data']);
-        $path = storage_path('app/' . $filename);
-        $image->save($path);
+        $content = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $attachment['data']));
+        $extension = pathinfo($attachment['name'], PATHINFO_EXTENSION);
+        $filename = str_random(32) . '.' . $extension;
+        Storage::put($filename, $content);
 
         $this->record_id = $attachment['record_id'];
         $this->name = $filename;
         $this->size = Storage::size($filename);
-
-        $image->destroy();
     }
 
     public function delete_file()
